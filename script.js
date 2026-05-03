259eafe365161eb751983d8f24644b13a12ffa9b
yosraelmasry-dev <74475192+yel-eng@users.noreply.github.com>
2026-05-03 03:24:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "mydoctor-clinic.firebaseapp.com",
+  projectId: "mydoctor-clinic",
+  storageBucket: "mydoctor-clinic.appspot.com",
+  messagingSenderId: "996532645974",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
 /******** BOOK ********/
 function book(){
   let name=document.getElementById("name").value;
